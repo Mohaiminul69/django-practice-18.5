5e7a3086d72a477f07d59444e190dcf96e235e7e
mohaiminul69 <mohaiminul69@gmail.com>
2024-11-22 19:46:16 +0600

    login logout done.

--- authentication/profile_app/urls.py
+++ authentication/profile_app/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from profile_app.views import home, sign_up
+from profile_app.views import home, sign_up, user_login, user_logout
 
 urlpatterns = [
     path("", home, name="homepage"),
-    path("sign_up", sign_up, name="signup_page"),
+    path("sign_up/", sign_up, name="signup_page"),
+    path("login/", user_login, name="login_page"),
+    path("logout/", user_logout, name="logout_page"),
 ]
--- authentication/profile_app/views.py
+++ authentication/profile_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
@@ -9,11 +11,40 @@ def home(request):
 
 
 def sign_up(request):
+    if request.user.is_authenticated:
+        return redirect("homepage")
     if request.method == "POST":
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, "User registered Successfully")
+            return redirect("login_page")
     else:
         form = RegistrationForm()
-    return render(request, "sign_up.html", {"form": form})
+    return render(request, "sign_up.html", {"form": form, "type": "Registration"})
+
+
+def user_login(request):
+    if request.user.is_authenticated:
+        return redirect("homepage")
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                messages.success(request, "Logged Successfully")
+                login(request, user)
+                return redirect("homepage")
+            else:
+                messages.warning(request, "Login information is not valid")
+                return redirect("signup_page")
+    else:
+        form = AuthenticationForm()
+    return render(request, "sign_up.html", {"form": form, "type": "Login"})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("login_page")
