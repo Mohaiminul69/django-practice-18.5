4d3e798c118a838ff11e71a91216db4a9357d00b
mohaiminul69 <mohaiminul69@gmail.com>
2024-11-22 18:43:25 +0600

    signup page added.

--- authentication/profile_app/urls.py
+++ authentication/profile_app/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from profile_app.views import home
+from profile_app.views import home, sign_up
 
 urlpatterns = [
     path("", home, name="homepage"),
+    path("sign_up", sign_up, name="signup_page"),
 ]
--- authentication/profile_app/views.py
+++ authentication/profile_app/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from .forms import RegistrationForm
+from django.contrib import messages
 
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+
+def sign_up(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "User registered Successfully")
+    else:
+        form = RegistrationForm()
+    return render(request, "sign_up.html", {"form": form})
